c451dd2d853000ea377dd9305b1eb3e5c631ee70
Diretnan Domnan <diretnandomnan@gmail.com>
2021-04-11 22:44:42 +0100

    still trying to figure out list workings

--- src/providers/aws.rs
+++ src/providers/aws.rs
@@ -1,7 +1,58 @@
+use async_trait::async_trait;
 use crate::types::bucket::{Buckets, Bucket};
-use crate::types::blob::{Blob};
+//use crate::types::blob::{Blob};
+use rusoto_core::{Region};
+use rusoto_s3::{S3, S3Client};
 
-pub struct AwsBuckets;
-pub struct AwsBucket;
+pub struct AwsBuckets{
+    s3: S3Client,
+}
+
+#[derive(Debug)]
+pub struct AwsBucket{
+    name: String,
+}
+
+impl AwsBucket {
+    fn new(name: String) -> Self {
+        AwsBucket {
+            name
+        }
+
+    }
+}
+
+#[derive(Debug)]
 pub struct AwsBlob;
 
+
+impl AwsBuckets {
+    fn new(region: Region) -> Self {
+        AwsBuckets {
+            s3: S3Client::new(region)
+        }
+
+    }
+
+}
+
+impl Bucket for AwsBucket {
+}
+
+#[async_trait]
+impl Buckets for AwsBuckets {
+    async fn list(&self) -> Box<Vec<AwsBucket>> {
+        let resp = self.s3.list_buckets().await.unwrap();
+        let mut buckets: Vec<AwsBucket> = Vec::new();
+        for bucket in resp.buckets.unwrap().iter() {
+            if bucket.name.is_some(){
+                let bucket_found = AwsBucket::new(
+                    String::from(bucket.name.clone().unwrap())
+                    );
+                buckets.push(bucket_found);
+            }
+        }
+        Box::new(buckets)
+    }
+
+}
--- src/types/bucket.rs
+++ src/types/bucket.rs
@@ -11,34 +11,33 @@ use crate::types::errors::{
 #[async_trait]
 pub trait Buckets {
     /// Open an existing bucket 
-    async fn open<T>(&self, bucket_name: String) -> BucketResult<T>
-        where T: Bucket;
-    /// Create a bucket
-    async fn create<T>(&self, bucket_name: String) -> BucketResult<T>
-        where T: Bucket;
+//    async fn open<T>(&self, bucket_name: String) -> BucketResult<T>
+//        where T: Bucket;
+//    /// Create a bucket
+//    async fn create<T>(&self, bucket_name: String) -> BucketResult<T>
+//        where T: Bucket;
     /// List all buckets
-    async fn list<T>(&self) -> Option<Vec<T>>
-        where T: Bucket;
-    /// Delete a bucket
-    async fn delete(&self, bucket_name: String) -> BucketResult<bool>;
-    /// Check if a bucket exists
-    async fn exists(&self, bucket_name: String) -> bool;
+    async fn list(&self) -> Box<Vec<dyn Bucket>>;
+//    /// Delete a bucket
+//    async fn delete(&self, bucket_name: String) -> BucketResult<bool>;
+//    /// Check if a bucket exists
+//    async fn exists(&self, bucket_name: String) -> bool;
 }
 
 /// Bucket delete single object, can create blob,
 /// delete blob and retrieve blob
 #[async_trait]
 pub trait Bucket {
-    /// Delete this particular bucket
-    async fn delete(&self) -> BucketResult<bool>;
-    /// Retrieve a blob from this bucket
-    async fn get_blob<T>(&self, blob_name: String) -> BlobResult<T>
-        where T: Blob;
-    /// Create a blob in bucket
-    async fn create_blob<T>(&self, blob_name: String) -> BlobResult<T>
-        where T: Blob;
-    /// Delete a blob from bucket
-    async fn delete_blob(&self, blob_name: String) -> BlobResult<bool>;
-    /// Check if a blob exists
-    async fn exists(&self, blob_name: String) -> bool;
+//    /// Delete this particular bucket
+//    async fn delete(&self) -> BucketResult<bool>;
+//    /// Retrieve a blob from this bucket
+//    async fn get_blob<T>(&self, blob_name: String) -> BlobResult<T>
+//        where T: Blob;
+//    /// Create a blob in bucket
+//    async fn create_blob<T>(&self, blob_name: String) -> BlobResult<T>
+//        where T: Blob;
+//    /// Delete a blob from bucket
+//    async fn delete_blob(&self, blob_name: String) -> BlobResult<bool>;
+//    /// Check if a blob exists
+//    async fn exists(&self, blob_name: String) -> bool;
 }
